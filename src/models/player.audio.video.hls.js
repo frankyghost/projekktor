6528d9a96cde47b4f7a915ec00b1e785d8ea6f2d
Radosław Włodkowski <radoslaw@wlodkowski.net>
2014-01-17 16:51:50 +0100

    Remove native platform which causing problems with proper HLS detection on mobile devices

--- src/models/player.audio.video.hls.js
+++ src/models/player.audio.video.hls.js
@@ -11,30 +11,32 @@ jQuery(function($) {
     
 $p.newModel({    
     modelId: 'VIDEOHLS',    
-    androidVersion: "4", // 
-    iosVersion: "3",
-    nativeVersion: "1",
+    androidVersion: 4,
+    iosVersion: 3,    
     iLove: [
-        {ext:'m3u8', type:'application/x-mpegURL', platform: ['ios', 'android', 'native'], streamType: ['http','httpVideo', 'httpVideoLive']},
-        {ext:'m3u', type:'application/x-mpegURL', platform: ['ios', 'android', 'native'], streamType: ['http', 'httpVideo', 'httpVideoLive']},   
-        {ext:'m3u8', type:'application/vnd.apple.mpegURL', platform: ['ios', 'android', 'native'], streamType: ['http','httpVideo', 'httpVideoLive']},
-        {ext:'m3u', type:'application/vnd.apple.mpegURL', platform: ['ios', 'android', 'native'], streamType: ['http', 'httpVideo', 'httpVideoLive']} 
+        {ext:'m3u8', type:'application/mpegURL', platform: ['ios', 'android'], streamType: ['http','httpVideo', 'httpVideoLive']},
+        {ext:'m3u', type:'application/mpegURL', platform: ['ios', 'android'], streamType: ['http', 'httpVideo', 'httpVideoLive']},
+        {ext:'m3u8', type:'application/vnd.apple.mpegURL', platform: ['ios', 'android'], streamType: ['http','httpVideo', 'httpVideoLive']},
+        {ext:'m3u', type:'application/vnd.apple.mpegURL', platform: ['ios', 'android'], streamType: ['http', 'httpVideo', 'httpVideoLive']},             
+        {ext:'m3u8', type:'application/x-mpegURL', platform: ['ios', 'android'], streamType: ['http','httpVideo', 'httpVideoLive']},
+        {ext:'m3u', type:'application/x-mpegURL', platform: ['ios', 'android'], streamType: ['http', 'httpVideo', 'httpVideoLive']}    
     ]
     
 }, 'VIDEO');
 
 $p.newModel({    
     modelId: 'AUDIOHLS',    
-    androidVersion: "4",
-    iosVersion: "3",
-    nativeVersion: "1",
+    androidVersion: 4,
+    iosVersion: 3,
     iLove: [
-        {ext:'m3u8', type:'application/x-mpegURL', platform: ['ios', 'android', 'native'], streamType: ['http','httpAudio', 'httpAudioLive']},
-        {ext:'m3u', type:'application/x-mpegURL', platform: ['ios', 'android', 'native'], streamType: ['http','httpAudio', 'httpAudioLive']},
-        {ext:'m3u8', type:'application/vnd.apple.mpegURL', platform: ['ios', 'android', 'native'], streamType: ['http','httpAudio', 'httpAudioLive']},
-        {ext:'m3u', type:'application/vnd.apple.mpegURL', platform: ['ios', 'android', 'native'], streamType: ['http','httpAudio', 'httpAudioLive']},                 
-        {ext:'m3u8', type:'audio/mpegURL', platform: ['ios', 'android', 'native'], streamType: ['http','httpAudio', 'httpAudioLive']},
-        {ext:'m3u', type:'audio/mpegURL', platform: ['ios', 'android', 'native'], streamType: ['http', 'httpAudio', 'httpAudioLive']}
+        {ext:'m3u8', type:'application/mpegURL', platform: ['ios', 'android'], streamType: ['http','httpAudio', 'httpAudioLive']},
+        {ext:'m3u', type:'application/mpegURL', platform: ['ios', 'android'], streamType: ['http','httpAudio', 'httpAudioLive']},
+        {ext:'m3u8', type:'application/vnd.apple.mpegURL', platform: ['ios', 'android'], streamType: ['http','httpAudio', 'httpAudioLive']},
+        {ext:'m3u', type:'application/vnd.apple.mpegURL', platform: ['ios', 'android'], streamType: ['http','httpAudio', 'httpAudioLive']},             
+        {ext:'m3u8', type:'application/x-mpegURL', platform: ['ios', 'android'], streamType: ['http','httpAudio', 'httpAudioLive']},
+        {ext:'m3u', type:'application/x-mpegURL', platform: ['ios', 'android'], streamType: ['http','httpAudio', 'httpAudioLive']},    
+        {ext:'m3u8', type:'audio/mpegURL', platform: ['ios', 'android'], streamType: ['http','httpAudio', 'httpAudioLive']},
+        {ext:'m3u', type:'audio/mpegURL', platform: ['ios', 'android'], streamType: ['http', 'httpAudio', 'httpAudioLive']}
     ]
     
 }, 'AUDIO');
