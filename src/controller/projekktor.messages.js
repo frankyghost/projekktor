108d998c68b943a23e8a958e7c88601470c0a76a
Sascha Kluger <frankyghost@googlemail.com>
2014-01-30 17:47:36 +0100

    major cleanup, adding settings menu

---
+++ src/controller/projekktor.messages.js
@@ -0,0 +1,48 @@
+projekktorMessages = {
+    
+    // controlbar 
+    "play": "start playback",
+    "pause": "pause playback",
+    
+    // settings 
+    "help": "help center",
+    "keyboard controls": "keyboard controls",
+    "debug": "debug",
+    "player info": "player info",
+    "platform": "platform",
+    "flash": "Flash",
+    "html5": "HTML5",
+    "vlc": "VLC Plugin",
+    "auto": "automatic",
+    "quality": "quality",
+    "high": "high",
+    "medium": "medium",
+    "low": "low",
+    
+    errorMessages:  {
+        /* flash & native: */
+        0: '#0 An (unknown) error occurred.',
+        1: '#1 You aborted the media playback. ',
+        2: '#2 A network error caused the media download to fail part-way. ',
+        3: '#3 The media playback was aborted due to a corruption problem. ',
+        4: '#4 The media (%{title}) could not be loaded because the server or network failed.',
+        5: '#5 Sorry, your browser does not support the media format of the requested file.',
+        6: '#6 Your client is in lack of the Flash Plugin V%{flashver} or higher.',
+        7: '#7 No media scheduled.',
+        8: '#8 ! Invalid media model configured !',
+        9: '#9 File (%{file}) not found.',
+        10: '#10 Invalid or missing quality settings for %{title}.',
+        11: '#11 Invalid streamType and/or streamServer settings for %{title}.',
+        12: '#12 Invalid or inconsistent quality setup for %{title}.',
+        80: '#80 The requested file does not exist or is delivered with an invalid content-type.',
+        97: 'No media scheduled.',
+        98: 'Invalid or malformed playlist data!',
+        99: 'Click display to proceed. ',
+        100: 'Keyboard Shortcuts',
+            
+        /* youtube errors: */
+        500: 'This Youtube video has been removed or set to private',
+        501: 'The Youtube user owning this video disabled embedding.',
+        502: 'Invalid Youtube Video-Id specified.'
+    }
+};
