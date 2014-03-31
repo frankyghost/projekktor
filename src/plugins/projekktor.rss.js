3aa1366478fd3e431ab5812ccb9c876115669857
Frankyghost <Frankyghost@googlemail.com>
2014-03-31 10:51:53 +0200

    added RSS parser plugin

---
+++ src/plugins/projekktor.rss.js
@@ -0,0 +1,62 @@
+/*
+ * Projekktor II Plugin: Contextmenu
+ *
+ * under GNU General Public License
+ * http://www.projekktor.com/license/
+ */
+var projekktorRSS = function(){};
+jQuery(function($) {
+projekktorRSS.prototype = {
+        
+    version: '1.1.00',
+    reqVer: '1.2.13', 
+    
+    initialize: function() {
+        this.pluginReady = true;
+    },
+    
+    scheduleLoadingHandler: function() {
+        this.pluginReady = false;
+    },
+    
+    scheduleLoadedHandler: function(xmlDocument) {
+        console.log("hier", arguments)
+        var node = null;
+        try {
+            node = $(xmlDocument).find("rss");
+            if (node.length>0) {
+                if (node.attr("version")==2) {
+                    this.pp.addParser(this.parse);
+                }
+            }
+            
+        } catch(e) {console.log(e)}
+        this.pluginReady = true;  
+    },
+    
+    parse: function(xmlDocument) {
+        var result = {},
+            itm=0;
+            
+        result.playlist = [];
+        
+        $(xmlDocument).find("item").each(function() {            
+            try {
+                result['playlist'].push({
+                    0:{
+                        src:  $(this).find('enclosure').attr('url'),            
+                        type: $(this).find('enclosure').attr('type')            
+                    },
+                    config: {
+                        poster: $(this).find('media\\:thumbnail').attr('url'),
+                        title: $(this).find('title').text(),
+                        desc: $(this).find('description').text()
+                    }
+                });
+            } catch(e){}
+        });
+        
+        return result;
+    }
+}
+});
